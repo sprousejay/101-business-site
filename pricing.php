cd190e747faa1eeb63d3d9a5a728b1627255c0b1
Jay Sprouse <jaysprouse2689@gmail.com>
2016-11-07 19:46:28 -0600

    wip

--- pricing.php
+++ pricing.php
@@ -19,26 +19,26 @@
 				<div class="sessions">4 SESSIONS</div>
 			</div>
 			<div>
-				<p>Our pre and postnatal massages have proven to be effective and absolutely safe, both for women and their unborn babies...</p>
+				<p class="description">Our pre and postnatal massages have proven to be effective and absolutely safe, both for women and their unborn babies...</p>
 			</div>
 			<div>
-				<a href="booking.php" class="nav__link--booking">Book an appointment</a>
+				<div class="booker"><a href="booking.php" class="nav__link--booking">Book an appointment</a></div>
 			</div>
 		</div>
 
-		<div class="prices">
+		<div class="special-prices">
 			<div> 
-				<div class="option">Intensive Massage Sessions</div>
+				<div class="special-prices__option">Intensive Massage Sessions</div>
 			</div>
 			<div>
-				<div class="cost">$400</div>
-				<div class="sessions">4 SESSIONS</div>
+				<div class="special-prices__cost">$400</div>
+				<div class="special-prices__sessions">4 SESSIONS</div>
 			</div>
 			<div>
-				<p>This massage category if our most popular one and for a good reason. It gives you a completely refreshing experince!</p>
+				<p class="special-prices__description">This massage category if our most popular one and for a good reason. It gives you a completely refreshing experince!</p>
 			</div>
 			<div>
-				<a href="booking.php" class="nav__link--booking">Book an appointment</a>
+				<div class="special-prices__booker"><a href="booking.php" class="special-prices__nav__link--booking">Book an appointment</a></div>
 			</div>
 		</div>
 
@@ -51,34 +51,66 @@
 				<div class="sessions">4 SESSIONS</div>
 			</div>
 			<div>
-				<p>Try our "Hot Stones" or "Stones and Sticks" massages, which are both fun and good for your overall health and wellbeing!</p>
+				<p class="description">Try our "Hot Stones" or "Stones and Sticks" massages, which are both fun and good for your overall health and wellbeing!</p>
 			</div>
 			<div>
-				<a href="booking.php" class="nav__link--booking">Book an appointment</a>
+				<div class="booker"><a href="booking.php" class="nav__link--booking">Book an appointment</a></div>
 			</div>
 		</div>
 	</div>
 
 	<div>
-	Call us now to book a relaxing massage session!
+		<div class="call">Call us now to book a relaxing massage session!</div>
 
-	555-123-2322
+		<div class="telephone">
+			<i class="fa fa-phone call__image"></i><span class="phone">555-123-2322</span>
+		</div>
 
-	Service Cost: $99.00
+		<div class="dinero">
+			<spanclass="service-cost">Service Cost:</span><span class="money">$99.00</span>
+		</div>
 
-	Duration 1h 20m
+		<div class="duration">Duration: 1h 20m</div>
 	</div>
 
 
-	<div>
-		What our happy customers say:
-		<i class="fa fa-phone contact__image"></i>
+	<div class="customers">
+		<div class="happy">What our happy customers say:</div>
+		
+		<div class="quote"><img src="images/quote.png"></div>
 
-		Men rarely acknowledge how much they love to get a massage... But I want to say, that without my afterwork Friday massage sessions here, my productivity would've been so much lower...
+
+		<div class="men">Men rarely acknowledge how much they love to get a massage... But I want to say, that without my afterwork Friday massage sessions here, my productivity would've been so much lower...</div>
 
 		Rhode Gentlebear
 	</div>
 
+	
+	<div class="here">See a full list of our services here:
+	</div>
+
+	
+
+	<div class="list">
+
+		<div class="list__image">
+			<img src="images/services-02.jpg">
+			<div class="style">Swedish Massage</div>
+		</div>
+
+		<div class="list__image">
+			<img src="images/index-13.jpg">
+			<div class="style">Deep Tissue Massage</div>
+		</div>
+
+		<div class="list__image">
+			<img src="images/services-04.jpg">
+			<div class="style">Hot Stones Therapy</div>
+		</div>
+
+		<div class="see">See all Services</div>
+		
+	</div>
 		
 
 
