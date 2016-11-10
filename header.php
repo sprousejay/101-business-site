b69d5258e5abc98b668edff5c9ebc878f78e13c4
Jay Sprouse <jaysprouse2689@gmail.com>
2016-11-09 18:39:52 -0600

    wip

--- header.php
+++ header.php
@@ -12,11 +12,16 @@
 			<img src="images/logo.png" class="header-image">
 		</a>
 		<div class="nav">
-			<a href="homepage.php" class="nav__link">Home</a>
-			<a href="pricing.php" class="nav__link">Pricing</a>
-			<a href="masseurs.php" class="nav__link">Masseurs</a>
-			<a href="services.php" class="nav__link">Services</a>
-			<a href="contacts.php" class="nav__link">Contacts</a>
+			<a href="homepage.php" class="nav__link"<?php isCurrentPage('
+			Home', $currentPage); ?>">Home</a>
+			<a href="pricing.php" class="nav__link"<?php isCurrentPage('
+			Pricing', $currentPage); ?>">Pricing</a>
+			<a href="masseurs.php" class="nav__link"<?php isCurrentPage('
+			Masseurs', $currentPage); ?>">Masseurs</a>
+			<a href="services.php" class="nav__link"<?php isCurrentPage('
+			Services', $currentPage); ?>">Services</a>
+			<a href="contacts.php" class="nav__link"<?php isCurrentPage('
+			Contacts', $currentPage); ?>">Contacts</a>
 			<a href="booking.php" class="nav__link--booking">Book an appointment</a>
 		</div>
 	</div>
