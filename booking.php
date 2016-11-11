e7bcb593c696669f07a8f00b3e56a98c9a6b1cc5
Jay Sprouse <jaysprouse2689@gmail.com>
2016-11-10 20:36:54 -0600

    working on calendar

--- booking.php
+++ booking.php
@@ -1,60 +1,114 @@
 
 <?php include 'header.php';?>
 
-<div class="month"> 
-  <ul>
-    <li class="prev">&#10094;</li>
-    <li class="next">&#10095;</li>
-    <li>
-      <div class="month">October 2016
-      </div>
-    </li>
-  </ul>
+
+<div class="month">
+		<div class="month__header">October 2016
+		</div>
+	</div>
+
+
+<div class="calendar">
+	<div class="weekdays">
+		<div class="weekdays__header">Sun
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li></li>
+				<li>2</li>
+				<li>9</li>
+				<li>16</li>
+				<li>23</li>
+				<li>30</li>
+			</ul>
+		</div>
+	</div>
+	<div class="weekdays">
+		<div class="weekdays__header">Mon
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li></li>
+				<li>3</li>
+				<li>10</li>
+				<li>17</li>
+				<li>24</li>
+				<li>31</li>
+			</ul>
+		</div>
+	</div>
+	<div class="weekdays">
+		<div class="weekdays__header">Tue
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li></li>
+				<li>4</li>
+				<li>11</li>
+				<li>28</li>
+				<li>25</li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+	<div class="weekdays">
+		<div class="weekdays__header">Wed
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li></li>
+				<li>5</li>
+				<li>12</li>
+				<li>19</li>
+				<li>26</li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+	<div class="weekdays">
+		<div class="weekdays__header">Thu
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li></li>
+				<li>6</li>
+				<li>13</li>
+				<li>20</li>
+				<li>27</li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+	<div class="weekdays">
+		<div class="weekdays__header">Fri
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li></li>
+				<li>7</li>
+				<li>14</li>
+				<li>21</li>
+				<li>28</li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+	<div class="weekdays">
+		<div class="weekdays__header">Sat
+		</div>
+		<div class="weekdays__dates">
+			<ul class="dots">
+				<li>1</li>
+				<li>8</li>
+				<li>15</li>
+				<li>22</li>
+				<li>29</li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+
 </div>
-
-<ul class="weekdays">
-  <li>Mon</li>
-  <li>Tue</li>
-  <li>Wed</li>
-  <li>Thu</li>
-  <li>Fri</li>
-  <li>Sat</li>
-  <li>Sun</li>
-</ul>
-
-<ul class="days"> 
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-  <li>4</li>
-  <li>5</li>
-  <li>6</li>
-  <li>7</li>
-  <li>8</li>
-  <li>9</li>
-  <li>10</li>
-  <li>11</li>
-  <li>12</li>
-  <li>13</li>
-  <li>14</li>
-  <li>15</li>
-  <li>16</li>
-  <li>17</li>
-  <li>18</li>
-  <li>19</li>
-  <li>20</li>
-  <li>21</li>
-  <li>22</li>
-  <li>23</li>
-  <li>24</li>
-  <li>25</li>
-  <li>26</li>
-  <li>27</li>
-  <li>28</li>
-  <li>29</li>
-  <li>30</li>
-  <li>31</li>
-</ul>
 
 
 
